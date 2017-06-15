ba73414127784e44ad94378bd31bca663ad35aba
Stuart hiller <stuarthiller@Stuarts-MBP.lan>
2017-06-15 14:11:26 -0700

    Finished needs a little clean up

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,12 +1,26 @@
-// Your code goes here
-var family = {};
 
-var addButton = document.getElementsByClassName("add");
-//
-addButton[0].onclick = function(e) {
-  e.preventDefault();
-  grabValues();
+if (document.body.addEventListener) {
+  document.body.addEventListener('click', myHandler, false);
+} else {
+  document.body.attachEvent('onclick', myHandler);
 };
+
+function myHandler(e){
+  e = e || window.event;
+  var target = e.target || e.srcElement;
+  if (target.className.match(/delete/)) {
+    e.preventDefault();
+    removeFamilyMemember(target);
+  } else if (target.className.match(/add/)) {
+    e.preventDefault();
+    grabValues();
+  } else if (target.type == "submit") {
+    e.preventDefault();
+    submitFamily();
+  }
+};
+
+var family = {};
 
 var grabValues = function() {
   var form = document.getElementsByTagName('form');
@@ -26,7 +40,14 @@ var clearValues = function(form) {
 
 var addFamilyMemember = function(age, smoker, relation) {
   member = {};
-  familyMember = Object.keys(family).length + 1
+  var keys = Object.keys(family);
+  var last;
+  if (keys.length == 0) {
+    last = 1
+  } else {
+    last = parseInt(keys[keys.length-1]) + 1;
+  };
+  familyMember = last;
   member['age'] = age.value
   if (smoker.checked) {
     member['smoker'] = true;
@@ -40,36 +61,42 @@ var addFamilyMemember = function(age, smoker, relation) {
 
 var validateFields = function(age, relation) {
   checkAge = parseFloat(age.value)
-  if (isInteger(checkAge) == false) {
+  if (isInteger(checkAge) == false || checkAge == false || checkAge < 1) {
     return false;
-  };
-  if (checkAge == false || checkAge < 1) {
-    return false;
-  };
+  }
   if (relation.value == "") {
     return false;
-  };
+  }
   return true;
-}
+};
 
 function isInteger(n) {
   return n === +n && n === (n|0);
-}
+};
 
 var addMemberToDom = function() {
   var div = document.createElement('div');
-  div.className = 'new-member';
+  div.className = familyMember;
   var divText = familyMember + '.' +'<br>' + 'Age: ' + member['age'] + '<br>'
-  + 'Relation: ' + member['relation']
-  div.innerHTML = '<span class="member">' + divText + '</span>';
+  + 'Relation: ' + member['relation'] + '<br>'
+  + '<button class="delete">delete</button>'
+  div.innerHTML = divText;
   document.body.appendChild(div);
-}
+};
 
-var removeFamilyMemember = function() {
-  // code here
-}
+var removeFamilyMemember = function(element) {
+  divClassName = parseInt(element.parentElement.className);
+  delete family[divClassName];
+  divRemove = element.parentElement;
+  divParent = element.parentElement.parentElement;
+  divParent.removeChild(divRemove);
+  submitFamily();
+};
 
 var submitFamily = function() {
   jsonFamily = JSON.stringify(family);
   console.log(jsonFamily);
-}
+  pre = document.getElementsByClassName("debug");
+  pre[0].innerHTML = ''
+  pre[0].innerHTML = jsonFamily;
+};
